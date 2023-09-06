3aafc0fdeaa5c0b8f9ea82a9c2e3c0ab1a455d2f
Daniel Richert <danielrichert62@gmail.com>
2023-09-06 17:02:10 -0500

    Adds login actions, cannot create yet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,34 +1,34 @@
 class SessionsController < ApplicationController
-  # def create
-  #   user = User.find_by(email: params[:email])
-  #   if user && user.authenticate(params[:password])
-  #     jwt = JWT.encode(
-  #       {
-  #         user_id: user.id, # the data to encode
-  #         exp: 24.hours.from_now.to_i, # the expiration time
-  #       },
-  #       Rails.application.credentials.fetch(:secret_key_base), # the secret key
-  #       "HS256" # the encryption algorithm
-  #     )
-  #     render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
-  #   else
-  #     render json: {}, status: :unauthorized
-  #   end
-  # end
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i, # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
 
   # def new
   #   render template: "sessions/new"
   # end
 
-  def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to "/"
-    else
-      redirect_to "/login"
-    end
-  end
+  # def create
+  #   user = User.find_by(email: params[:email])
+  #   if user && user.authenticate(params[:password])
+  #     session[:user_id] = user.id
+  #     redirect_to "/"
+  #   else
+  #     redirect_to "/login"
+  #   end
+  # end
 
   def destroy
     session[:user_id] = nil
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,34 +14,34 @@ class UsersController < ApplicationController
     render template: "users/new"
   end
 
-  # def create
-  #   user = User.new(
-  #     name: params[:name],
-  #     email: params[:email],
-  #     password: params[:password],
-  #     password_confirmation: params[:password_confirmation],
-  #   )
-  #   if user.save
-  #     render json: { message: "User created successfully" }, status: :created
-  #   else
-  #     render json: { errors: user.errors.full_messages }, status: :bad_request
-  #   end
-  # end
-
   def create
-    @user = User.new(
-      name: params[:user][:name],
-      email: params[:user][:email],
-      password: params[:user][:password],
-      password_confirmation: params[:user][:password_confirmation],
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
     )
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to "/"
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
     else
-      render :new, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  # def create
+  #   @user = User.new(
+  #     name: params[:user][:name],
+  #     email: params[:user][:email],
+  #     password: params[:user][:password],
+  #     password_confirmation: params[:user][:password_confirmation],
+  #   )
+  #   if @user.save
+  #     session[:user_id] = @user.id
+  #     redirect_to "/"
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
   def destroy
     @user = User.find_by(id: params[:id])
